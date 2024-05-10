4322ed29985a227223780befa4bce48787e7a393
Sahil <sahilsharma86600@gmail.com>
2024-05-10 22:01:07 +0530

    checkout page

---
+++ data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    name: "Quilboat premium",
+    price: 100,
+    img: "assets/1-removebg-preview (1).png",
+    priceRange: "Rs. 1,000 - Rs. 10,000",
+    description: ["devices", "Ai", "style", "snippets", "instant access"],
+  },
+  {
+    id: 2,
+    name: "Chatgpt premium",
+    img: "assets/1-removebg-preview (1).png",
+
+    priceRange: "Rs. 1,000 - Rs. 10,000",
+    description: ["devices", "Ai", "style", "snippets", "instant access"],
+  },
+  {
+    id: 3,
+    name: "Canva premium",
+    img: "assets/1-removebg-preview (1).png",
+
+    priceRange: "Rs. 1,000 - Rs. 10,000",
+    description: ["devices", "Ai", "style", "snippets", "instant access"],
+  },
+];
+
+export default data;
