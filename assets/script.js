e77c46c29549b0a6d9d1e4f144a13469a16470da
Sahil <sahilsharma86600@gmail.com>
2024-05-18 16:02:42 +0530

    about us section add

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,23 @@ window.onscroll = function () {
 };
 
 function myFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    // document.getElementById("myImg").className = "slideUp";
-    // console.log(8);
-    // console.log(document.body.scrollTop);
-    // console.log(document.documentElement.scrollTop);
-
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
     document.querySelector(".navbar").classList.add("bg-black");
-    // document.querySelectorAll(".nav-link")[0].classList.add("text-white");
-    // document.querySelectorAll(".nav-link")[1].classList.add("text-white");
-    // document.querySelectorAll(".nav-link")[2].classList.add("text-white");
+    document.querySelector("header").classList.add("sticky-lg-top");
+    document.querySelector("header").style.transition = "all 0.5s ease 10s";
     const navLinks = document.querySelectorAll(".nav-link");
     navLinks.forEach((link) => {
       link.classList.add("text-white");
     });
   } else {
     document.querySelector(".navbar").classList.remove("bg-black");
+    document.querySelector("header").classList.remove("sticky-lg-top");
+    // document.querySelector("header").classList.remove("fixed-lg-top");
+    document.querySelector("header").style.transition = "";
+
     const navLinks = document.querySelectorAll(".nav-link");
     navLinks.forEach((link) => {
       link.classList.remove("text-white");
